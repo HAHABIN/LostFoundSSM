73de78a5878a44645ab62dc2bb89981da63d5bfb
HABIN <739115041@qq.com>
2020-03-01 01:31:09 +0800

    新增阿里云OSS 文件上传工具 修改图片上传模块 返回阿里云OSS地址

--- src/main/java/controller/UpdatePhoto.java
+++ src/main/java/controller/UpdatePhoto.java
@@ -1,10 +1,14 @@
 package controller;
 
+import com.aliyun.oss.OSSClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import sun.misc.BASE64Decoder;
+import utils.AliyunOSSClientUtil;
 import utils.HttpServletRequestUtil;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +17,9 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import static utils.OSSClientConstants.BACKET_NAME;
+import static utils.OSSClientConstants.FOLDER;
 
 /**
  * @author HABIN
@@ -55,40 +62,10 @@ public class UpdatePhoto {
 
         if (imageByte.length>0) {
             try {
-                //虚拟路径
-                String rootpath = "upload/image";
-                //获取文件上传的真实路径
-                String localpath = "D:\\upload\\image";
-                //保存目录文件的路径
-                String filepath = "/" + createNewDir();
-                //新建目录
-                File destfile = new File(localpath + filepath);
-                //如果文件目录不存在就新建
-                if (!destfile.exists()) {
-                    destfile.mkdirs();
-                }
-                //文件新名称
-                String fileNameNew = getFileNameNew() + ".jpeg";
-                //
-                File f = new File(destfile.getAbsoluteFile() + "/" + fileNameNew);
-                // 将字符串转换成二进制，用于显示图片
-                // 将上面生成的图片格式字符串 imgStr，还原成图片显示
-                InputStream in = new ByteArrayInputStream(imageByte);
-                FileOutputStream fos = new FileOutputStream(f);
-                byte[] buf = new byte[1024];
-                int length;
-                length = in.read(buf, 0, buf.length);
-
-                while (length != -1) {
-                    fos.write(buf,0,length);
-                    length = in.read(buf);
-                }
-                fos.flush();
-                fos.close();
-                in.close();
-                String lastpath = rootpath + filepath + "/" + fileNameNew;
-                System.out.println("返回图片路径：" + lastpath);
-
+//                String lastpath = getLocalImg(imageByte);
+                //初始化OSSClient
+                OSSClient ossClient=AliyunOSSClientUtil.getOSSClient();
+                String lastpath = AliyunOSSClientUtil.uploadObject1OSS(ossClient,imageByte,BACKET_NAME,FOLDER);
                 modelMap.put("success", true);
                 modelMap.put("message", "上传成功");
                 modelMap.put("code", 1);
@@ -105,6 +82,43 @@ public class UpdatePhoto {
         }
 
         return modelMap;
+    }
+
+    private String getLocalImg(byte[] imageByte) throws IOException {
+        //虚拟路径
+        String rootpath = "upload/image";
+        //获取文件上传的真实路径
+        String localpath = "D:\\upload\\image";
+        //保存目录文件的路径
+        String filepath = "/" + createNewDir();
+        //新建目录
+        File destfile = new File(localpath + filepath);
+        //如果文件目录不存在就新建
+        if (!destfile.exists()) {
+            destfile.mkdirs();
+        }
+        //文件新名称
+        String fileNameNew = getFileNameNew() + ".jpeg";
+        //
+        File f = new File(destfile.getAbsoluteFile() + "/" + fileNameNew);
+        // 将字符串转换成二进制，用于显示图片
+        // 将上面生成的图片格式字符串 imgStr，还原成图片显示
+        InputStream in = new ByteArrayInputStream(imageByte);
+        FileOutputStream fos = new FileOutputStream(f);
+        byte[] buf = new byte[1024];
+        int length;
+        length = in.read(buf, 0, buf.length);
+
+        while (length != -1) {
+            fos.write(buf,0,length);
+            length = in.read(buf);
+        }
+        fos.flush();
+        fos.close();
+        in.close();
+        String lastpath = rootpath + filepath + "/" + fileNameNew;
+        System.out.println("返回图片路径：" + lastpath);
+        return lastpath;
     }
 
 
